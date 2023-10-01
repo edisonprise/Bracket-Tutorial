10f5e483c319a79126706d1d9d4332d56033ec47
Edinson <edisonprise@hotmail.com>
2023-10-01 15:31:12 -0500

    final commit

--- Main.js
+++ Main.js
@@ -4,10 +4,9 @@ const query = encodeURIComponent("Select A");
 url = url + "&tq=" + query;
 
 fetch(url)
-  .then(res => res.text())
-  .then(rep => {
+  .then((res) => res.text())
+  .then((rep) => {
     const data = JSON.parse(rep.substr(47).slice(0, -2));
-    console.log(data);
 
     const row01 = document.createElement("tr");
     Player1.append(row01);
@@ -101,3 +100,173 @@ fetch(url)
       });
     });
   });
+
+const Result00 = document.getElementById("Result0");
+//------------------------------Box 1--------------------------------//
+{
+  const Res01 = document.getElementById("Result1");
+  const Res02 = document.getElementById("Result2");
+
+  const Name01 = document.getElementById("Player1");
+  const Name02 = document.getElementById("Player2");
+
+  Res01.addEventListener("change", () => {
+    let Result01 = parseInt(Res01.value);
+
+    if (Result01 === parseInt(Result00.value)) {
+      document.getElementById("Player9").innerHTML = Name01.value;
+    } else if (Result01 != parseInt(Result00.value)) {
+      document.getElementById("Player9").innerHTML = "";
+    }
+  });
+
+  Res02.addEventListener("change", () => {
+    let Result02 = parseInt(Res02.value);
+
+    if (Result02 === parseInt(Result00.value)) {
+      document.getElementById("Player9").innerHTML = Name02.value;
+    } else if (Result02 != parseInt(Result00.value)) {
+      document.getElementById("Player9").innerHTML = "";
+    }
+  });
+}
+//------------------------------Box 2--------------------------------//
+{
+  const Res01 = document.getElementById("Result3");
+  const Res02 = document.getElementById("Result4");
+
+  const Name01 = document.getElementById("Player3");
+  const Name02 = document.getElementById("Player4");
+
+  Res01.addEventListener("change", () => {
+    let Result01 = parseInt(Res01.value);
+
+    if (Result01 === parseInt(Result00.value)) {
+      document.getElementById("Player10").innerHTML = Name01.value;
+    } else if (Result01 != parseInt(Result00.value)) {
+      document.getElementById("Player10").innerHTML = "";
+    }
+  });
+
+  Res02.addEventListener("change", () => {
+    let Result02 = parseInt(Res02.value);
+
+    if (Result02 === parseInt(Result00.value)) {
+      document.getElementById("Player10").innerHTML = Name02.value;
+    } else if (Result02 != parseInt(Result00.value)) {
+      document.getElementById("Player10").innerHTML = "";
+    }
+  });
+}
+//------------------------------Box 3--------------------------------//
+{
+  const Res01 = document.getElementById("Result5");
+  const Res02 = document.getElementById("Result6");
+
+  const Name01 = document.getElementById("Player5");
+  const Name02 = document.getElementById("Player6");
+
+  Res01.addEventListener("change", () => {
+    let Result01 = parseInt(Res01.value);
+
+    if (Result01 === parseInt(Result00.value)) {
+      document.getElementById("Player11").innerHTML = Name01.value;
+    } else if (Result01 != parseInt(Result00.value)) {
+      document.getElementById("Player11").innerHTML = "";
+    }
+  });
+
+  Res02.addEventListener("change", () => {
+    let Result02 = parseInt(Res02.value);
+
+    if (Result02 === parseInt(Result00.value)) {
+      document.getElementById("Player11").innerHTML = Name02.value;
+    } else if (Result02 != parseInt(Result00.value)) {
+      document.getElementById("Player11").innerHTML = "";
+    }
+  });
+}
+//------------------------------Box 4--------------------------------//
+{
+  const Res01 = document.getElementById("Result7");
+  const Res02 = document.getElementById("Result8");
+
+  const Name01 = document.getElementById("Player7");
+  const Name02 = document.getElementById("Player8");
+
+  Res01.addEventListener("change", () => {
+    let Result01 = parseInt(Res01.value);
+
+    if (Result01 === parseInt(Result00.value)) {
+      document.getElementById("Player12").innerHTML = Name01.value;
+    } else if (Result01 != parseInt(Result00.value)) {
+      document.getElementById("Player12").innerHTML = "";
+    }
+  });
+
+  Res02.addEventListener("change", () => {
+    let Result02 = parseInt(Res02.value);
+
+    if (Result02 === parseInt(Result00.value)) {
+      document.getElementById("Player12").innerHTML = Name02.value;
+    } else if (Result02 != parseInt(Result00.value)) {
+      document.getElementById("Player12").innerHTML = "";
+    }
+  });
+}
+//------------------------------semi finals Box 1--------------------------------//
+{
+  const Res01 = document.getElementById("Result9");
+  const Res02 = document.getElementById("Result10");
+
+  const Name01 = document.getElementById("Player9");
+  const Name02 = document.getElementById("Player10");
+
+  Res01.addEventListener("change", () => {
+    let Result01 = parseInt(Res01.value);
+
+    if (Result01 === parseInt(Result00.value)) {
+      document.getElementById("Player13").innerHTML = Name01.innerHTML;
+    } else if (Result01 != parseInt(Result00.value)) {
+      document.getElementById("Player13").innerHTML = "";
+    }
+  });
+
+  Res02.addEventListener("change", () => {
+    let Result02 = parseInt(Res02.value);
+
+    if (Result02 === parseInt(Result00.value)) {
+      document.getElementById("Player13").innerHTML = Name02.innerHTML;
+    } else if (Result02 != parseInt(Result00.value)) {
+      document.getElementById("Player13").innerHTML = "";
+    }
+  });
+}
+//------------------------------semi finals Box 2--------------------------------//
+{
+  const Res01 = document.getElementById("Result11");
+  const Res02 = document.getElementById("Result12");
+
+  const Name01 = document.getElementById("Player11");
+  const Name02 = document.getElementById("Player12");
+
+  Res01.addEventListener("change", () => {
+    let Result01 = parseInt(Res01.value);
+
+    if (Result01 === parseInt(Result00.value)) {
+      document.getElementById("Player14").innerHTML = Name01.innerHTML;
+    } else if (Result01 != parseInt(Result00.value)) {
+      document.getElementById("Player14").innerHTML = "";
+    }
+  });
+
+  Res02.addEventListener("change", () => {
+    let Result02 = parseInt(Res02.value);
+
+    if (Result02 === parseInt(Result00.value)) {
+      document.getElementById("Player14").innerHTML = Name02.innerHTML;
+    } else if (Result02 != parseInt(Result00.value)) {
+      document.getElementById("Player14").innerHTML = "";
+    }
+  });
+}
